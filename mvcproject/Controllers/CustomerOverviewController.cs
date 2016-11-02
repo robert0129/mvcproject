0beacef978fe90ef142a02fc6bfd998261e9e53d
chaolong.chen <chaolong.chen@gmail.com>
2016-11-03 00:57:17 +0800

    Install NPOI Package and export the overview data to excel

--- mvcproject/Controllers/CustomerOverviewController.cs
+++ mvcproject/Controllers/CustomerOverviewController.cs
@@ -1,6 +1,7 @@
 ﻿using mvcproject.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -52,6 +53,36 @@ namespace mvcproject.Controllers
             repo.UnitOfWork.LazyLoadingEnabled = false;
             var data = repo.All().OrderBy(p => p.客戶名稱).ToList();
             return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        public FileResult ExportOverview()
+        {
+            //string schoolname = "401";
+            //创建Excel文件的对象
+            NPOI.HSSF.UserModel.HSSFWorkbook book = new NPOI.HSSF.UserModel.HSSFWorkbook();
+            //添加一个sheet
+            NPOI.SS.UserModel.ISheet sheet1 = book.CreateSheet("Sheet1");
+            //获取list数据
+            var overview = repo.All().ToList();
+            //List<TB_STUDENTINFOModel> listRainInfo = m_BLL.GetSchoolListAATQ(schoolname);
+            //给sheet1添加第一行的头部标题
+            NPOI.SS.UserModel.IRow row1 = sheet1.CreateRow(0);
+            row1.CreateCell(0).SetCellValue("客戶名稱");
+            row1.CreateCell(1).SetCellValue("聯絡人數量");
+            row1.CreateCell(2).SetCellValue("銀行帳戶數量");
+            //将数据逐步写入sheet1各个行
+            for (int i = 0; i < overview.Count; i++)
+            {
+                NPOI.SS.UserModel.IRow rowtemp = sheet1.CreateRow(i + 1);
+                rowtemp.CreateCell(0).SetCellValue(overview[i].客戶名稱.ToString());
+                rowtemp.CreateCell(1).SetCellValue(overview[i].聯絡人數量.ToString());
+                rowtemp.CreateCell(2).SetCellValue(overview[i].銀行帳戶數量.ToString());
+            }
+            // 写入到客户端 
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            book.Write(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            return File(ms, "application/vnd.ms-excel", "CustomersOverView.xls");
         }
     }
 }
